b700a72b4e606cb5b8c7e17254130818ea3a3d99
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-02-08 15:10:33 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,14 +1,17 @@
 set env vars
 set -o allexport; source .env; set +o allexport;
 
-cat << EOT >> ./config/wazuh_dashboard/wazuh.yml
+# cat << EOT >> ./config/wazuh_dashboard/wazuh.yml
 
-hosts:
-  - 1513629884013:
-      url: "https://wazuh.manager"
-      port: 55000
-      username: admin
-      password: "${ADMIN_PASSWORD}"
-      run_as: false
+# hosts:
+#   - 1513629884013:
+#       url: "https://wazuh.manager"
+#       port: 55000
+#       username: admin
+#       password: "${ADMIN_PASSWORD}"
+#       run_as: false
 
-EOT
+# EOT
+
+sysctl -w vm.max_map_count=262144
+docker-compose -f generate-indexer-certs.yml run --rm generator
