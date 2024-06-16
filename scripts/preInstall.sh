7531c31c0add915a24f2d7ed1c11798aefca0a08
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-06-16 15:51:20 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -13,5 +13,21 @@ set -o allexport; source .env; set +o allexport;
 
 # EOT
 
+mkdir -p ./wazuh-indexer-data
+mkdir -p ./wazuh-dashboard-config
+mkdir -p ./wazuh-dashboard-custom
+
+chown -R 777 ./wazuh-indexer-data
+chown -R 777 ./wazuh-dashboard-config
+chown -R 777 ./wazuh-dashboard-custom
+
+chmod -R 777 ./wazuh-indexer-data
+chmod -R 777 ./wazuh-dashboard-config
+chmod -R 777 ./wazuh-dashboard-custom
+
+chmod o+r ./wazuh-indexer-data
+chmod o+r ./wazuh-dashboard-config
+chmod o+r ./wazuh-dashboard-custom
+
 sysctl -w vm.max_map_count=262144
 docker-compose -f generate-indexer-certs.yml run --rm generator
