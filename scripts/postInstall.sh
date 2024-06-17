d39e379b99272d5e2f214ecddd667e5894f38d17
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-06-17 18:12:54 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,11 +3,3 @@ set -o allexport; source .env; set +o allexport;
 
 echo "Waiting for software to be ready..."
 sleep 120s;
-
-sed -i "s~SecretPassword~${BCRYPT_HASH}~g" ./filebeat_etc/filebeat.yml
-
-
-docker-compose down;
-docker-compose up -d;
-
-sleep 120s;
