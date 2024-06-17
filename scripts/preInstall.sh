0e462a257f34551e03daa7a2a96ca3c7f4db953d
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-06-17 16:11:38 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,17 +1,6 @@
 set env vars
 set -o allexport; source .env; set +o allexport;
 
-# cat << EOT >> ./config/wazuh_dashboard/wazuh.yml
-
-# hosts:
-#   - 1513629884013:
-#       url: "https://wazuh.manager"
-#       port: 55000
-#       username: admin
-#       password: "${ADMIN_PASSWORD}"
-#       run_as: false
-
-# EOT
 
 mkdir -p ./wazuh-indexer-data
 mkdir -p ./wazuh-dashboard-config
@@ -21,13 +10,11 @@ chown -R 777 ./wazuh-indexer-data
 chown -R 777 ./wazuh-dashboard-config
 chown -R 777 ./wazuh-dashboard-custom
 
-chmod -R 777 ./wazuh-indexer-data
-chmod -R 777 ./wazuh-dashboard-config
-chmod -R 777 ./wazuh-dashboard-custom
-
 chmod o+r ./wazuh-indexer-data
 chmod o+r ./wazuh-dashboard-config
 chmod o+r ./wazuh-dashboard-custom
+
+chmod +x ./generate-password.sh
 
 sysctl -w vm.max_map_count=262144
 docker-compose -f generate-indexer-certs.yml run --rm generator
